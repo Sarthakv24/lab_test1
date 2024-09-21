c3675aab4fecc2d028ccfd9c5deb665e074d25b5
Sarthakv24 <151907539+Sarthakv24@users.noreply.github.com>
2024-09-21 10:50:41 +0530

    Create test1_Q1.py

---
+++ test1_Q1.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+Inventory = np.array([
+    ['DOLO' ,30 ,100]
+    ['AZITHROMICIN' ,50 ,150]
+    ['PARACITAMOL' ,20 ,80]
+    ['MAPTAL' ,40 ,101]
+    ['PAN40',33 , 100]
+])
+
+    
+def Chemist_shop:
+      print ("MAIN MENU")
+      print ("1.Add_New_Item")
+      print ("2.Update_Medicine_Quanties")
+      print ("3.Calculate_Total sale")
+      print("4.Calculate Profit")
+      print ("5.Display the current Inventory")
+      print ("6.Graph for Total and profit")
+      print ("7.Display current inventory")
+      print ("8.Exit the Menu")
+ 
+def Add_New_Items(name , price , Quantity):
+    
+    
+    
+def Update_Medicine_Quanties :
+
+def calculate_Total_sale:
+
+def Calculate_profit:
+
+
+
+   
+int main
